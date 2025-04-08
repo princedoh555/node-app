e78c4ab53dab84928b247cde91d2322583faf492
princedoh555 <lawrencedoh193@gmail.com>
2025-04-08 01:59:31 -0500

    delete is working but commented out

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -42,9 +42,24 @@ export const update = async(req, res) => {
         }
 
         const updateEmployee = await Employee.findByIdAndUpdate(id, req.body, {new: true})
-        res.status(201).json(updateEmployee)
-        
+        res.status(201).json(updateEmployee)    
     } catch (error) {
         res.status(500).json({error: "Internal Server Error"})  
     }
 }
+
+// export const deleteEmployee = async(req, res) => {
+//     try {
+//         const id = req.params.id
+//         const employeeExist = await Employee.findOne({_id:id})
+
+//         if(!employeeExist) {
+//             return res.status(404).json({message: "Employee not found" })
+//         }
+
+//         await Employee.findByIdAndUpdate(id)
+//         res.status(201).json({message: "Employee terminated!!"})    
+//     } catch (error) {
+//         res.status(500).json({error: "Internal Server Error"})  
+//     }
+// }
